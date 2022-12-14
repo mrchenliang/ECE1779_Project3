c7450cf5db953f7c949b33963076ece6da2b20ce
BrianQu <qujianning0401@163.com>
2022-12-14 17:07:39 -0500

    update main.py

--- A_3/frontend/main.py
+++ A_3/frontend/main.py
@@ -32,15 +32,15 @@ def teardown_db(exception):
 def home():
     return render_template('main.html')
 
-@webapp.errorhandler(404)
-# returns the 404 page
-def page_not_found(e):
-    return render_template('404.html'), 404
+# @webapp.errorhandler(404)
+# # returns the 404 page
+# def page_not_found(e):
+#     return render_template('404.html'), 404
 
-@webapp.errorhandler(500)
-# returns the 500 page
-def internal_server_error(e):
-    return render_template('500.html'), 500
+# @webapp.errorhandler(500)
+# # returns the 500 page
+# def internal_server_error(e):
+#     return render_template('500.html'), 500
 
 if __name__ == '__main__':
     webapp.run('0.0.0.0',5000,debug=True,threaded=True)
