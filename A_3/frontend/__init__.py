49eb14b0bddf14cde27dbf43f190048988819f37
BrianQu <qujianning0401@163.com>
2022-12-14 15:26:05 -0500

    update the init.py

--- A_3/frontend/__init__.py
+++ A_3/frontend/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from frontend.api import api_routes
+from frontend.image import image_routes
+from frontend.manager import manager_routes
+from frontend.stat import stat_routes
+
 
 webapp = Flask(__name__)
+webapp.register_blueprint(api_routes)
+webapp.register_blueprint(image_routes)
+webapp.register_blueprint(manager_routes)
+webapp.register_blueprint(stat_routes)
--- A_3/frontend/main.py
+++ A_3/frontend/main.py
@@ -1,23 +1,13 @@
 from glob import glob
 from flask import Flask, render_template, g
-from frontend.constants import default_max_capacity, default_replacement_policy
-from frontend.manager.manager_helper import set_cache
-from frontend.api import api_routes
-from frontend.image import image_routes
-from frontend.manager import manager_routes
-from frontend.stat import stat_routes
+# from frontend.constants import default_max_capacity, default_replacement_policy
+# from frontend.manager.manager_helper import set_cache
 from frontend import webapp
-
-webapp.register_blueprint(api_routes)
-webapp.register_blueprint(image_routes)
-webapp.register_blueprint(manager_routes)
-webapp.register_blueprint(stat_routes)
 
 # @webapp.before_first_request
 # # initialize the cache configuration settings on first startup
 # def set_cache_config_settings():
 #     set_cache(default_max_capacity, default_replacement_policy)
-
 
 @webapp.teardown_appcontext
 # close out the db connection on shutdown
