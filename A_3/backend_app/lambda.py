436ae916bf50f5832a40f50d38bbe282c70e314d
BrianQu <qujianning0401@163.com>
2022-12-20 14:07:18 -0500

    update

--- A_3/backend_app/lambda.py
+++ A_3/backend_app/lambda.py
@@ -1,5 +1,5 @@
 import boto3
-import json
+import json, requests, time
 
 lambda_client = boto3.client('lambda',
                              region_name='us-east-1',)
@@ -9,3 +9,8 @@ response = lambda_client.invoke(FunctionName="cloudwatch",
                                 InvocationType="Event",         ###for asynchronous purposes
                                 Payload=json.dumps(data)
                                 )
+
+url = "https://msiadrxh4hg334bvmxhs7n6ija0jqwog.lambda-url.us-east-1.on.aws/ "
+while True:
+    res = requests.get(url, params=data)
+    time.sleep(5)
